07f3cd0ea8c925a4c49480b46e89d527565ec66f
Matthew Geddes <musicalcarrion@gmail.com>
2023-10-28 23:50:13 -0700

    Fixed build on non-Linux

--- src/unsupported.rs
+++ src/unsupported.rs
@@ -7,7 +7,7 @@ use crate::Roameo;
 use std::io::{Error, ErrorKind};
 
 impl Roameo {
-    pub fn match_essid(&self) -> Result<(), Error> {
+    pub fn match_essid(&self, _essid: &str) -> Result<(), Error> {
         Err(Error::new(
             ErrorKind::Other,
             "ESSID match support not yet available on this platform",
